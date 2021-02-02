41501a092ff3b97e365957e5af663bd3a9c9b8a1
Jesús Millán <jgmc3012@gmail.com>
2021-02-01 20:39:56 -0400

    :star:[ADD] peak.html

---
+++ l5_peak.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+def l5():
+    import pickle
+    import requests
+    resp = requests.get("http://www.pythonchallenge.com/pc/def/banner.p")
+    data = pickle.loads(bytes(resp.text, "UTF-8"))
+    for line_raw in data:
+        line = ""
+        for chars in line_raw:
+            line += chars[0]*chars[1]
+        print(line)
+       
+            
